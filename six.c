17b0d6aa43e5be463f634fe7502799bc694d3fc4
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-09 22:50:30 +0530

    Update six.c

--- six.c
+++ six.c
@@ -2,7 +2,6 @@
 int main()
 {
     int year;
-    printf("Enter a year: ");
     scanf("%d",&year);
     if(year%4 == 0)
     {
@@ -10,15 +9,15 @@ int main()
         {
             // year is divisible by 400, hence the year is a leap year
             if ( year%400 == 0)
-                printf("%d is a leap year.", year);
+                printf("yes");
             else
-                printf("%d is not a leap year.", year);
+                printf("no");
         }
         else
-            printf("%d is a leap year.", year );
+            printf("yes");
     }
     else
-        printf("%d is not a leap year.", year);
+        printf("no");
     
     return 0;
 }
