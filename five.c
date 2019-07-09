5e3f607eabd7dc941ec439167f5b6afa66a5cac4
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-09 22:40:53 +0530

    Update five.c

--- five.c
+++ five.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    double a, b, c;
-    scanf("%lf %lf %lf", &a, &b, &c);
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
     if( a>=b && a>=b )
-        printf("%.2f is the largest number.", a);
+        printf("%d", a);
     if(b>=a && b>=c )
-        printf("%.2f is the largest number.", b);
+        printf("%d ", b);
     if( c>=a && c>=b )
-        printf("%.2f ", c);
+        printf("%d ", c);
     return 0;
 }
