5fed81a5b7d1e548c87052fe748818484f400b31
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-09 22:28:03 +0530

    Update three.c

--- three.c
+++ three.c
@@ -3,8 +3,10 @@ int main()
 {
     char a;
     scanf("%c",&a);
-    if(a='a'||'e'||'i'||'o'||'u')
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
       printf("Vowel");
-    else
+    else if((a>=65&&a<91)||(a>=97&&a<=122))
        printf("Consonant");
+    else
+        printf("invalid");
  }
