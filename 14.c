9205edfbcc18713225a0143964d9d6412044e7de
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-12 10:58:22 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,q,odd=0;
+    scanf("%d%d",&n,&q);
+    for(i=n;i<q;i++)
+    {
+        if(i%2!=0)
+            odd++;
+    }
+     printf("%d",odd);
+}
