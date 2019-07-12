0b3d38345df5239c2e33d3e9adeb1e29a8ef21e3
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-12 11:01:28 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -3,10 +3,13 @@ int main()
 {
     int i,n,q,odd=0;
     scanf("%d%d",&n,&q);
+    if(n<=100000&&q<=100000)
+    {
     for(i=n;i<=q;i++)
     {
         if(i%2!=0)
             odd++;
     }
      printf("%d",odd);
+    }
 }
